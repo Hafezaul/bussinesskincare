dc7f1be7d54bf99f2b346321a5d0175afcf2587a
John Doe <faris.al.afi@gmail.com>
2024-11-17 17:35:32 +0700

    rampung rek

--- main.js
+++ main.js
@@ -1,33 +1,25 @@
-// Modal 1
-AOS.init();
-document.getElementById('modalButton1').addEventListener('click', function() {
-  document.getElementById('myModal1').classList.add('is-active');
-});
-document.getElementById('closeModal1').addEventListener('click', function() {
-  document.getElementById('myModal1').classList.remove('is-active');
-});
-document.getElementById('cancelModal1').addEventListener('click', function() {
-  document.getElementById('myModal1').classList.remove('is-active');
-});
-
-// Modal 2
-document.getElementById('modalButton2').addEventListener('click', function() {
-  document.getElementById('myModal2').classList.add('is-active');
-});
-document.getElementById('closeModal2').addEventListener('click', function() {
-  document.getElementById('myModal2').classList.remove('is-active');
-});
-document.getElementById('cancelModal2').addEventListener('click', function() {
-  document.getElementById('myModal2').classList.remove('is-active');
-});
-
-// Modal 3
-document.getElementById('modalButton3').addEventListener('click', function() {
-  document.getElementById('myModal3').classList.add('is-active');
-});
-document.getElementById('closeModal3').addEventListener('click', function() {
-  document.getElementById('myModal3').classList.remove('is-active');
-});
-document.getElementById('cancelModal3').addEventListener('click', function() {
-  document.getElementById('myModal3').classList.remove('is-active');
-});
+function handleBuy() {
+  Swal.fire({
+    title: "Apakah Anda Yakin?",
+    text: "Ingin membelinya",
+    icon: "question",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    confirmButtonText: "Ya, beli"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire({
+        title: "Jika anda yakin",
+        text: "Maka akan kami arahkan ke admin.",
+        icon: "info",
+        showCancelButton: false,
+        showConfirmButton: true,
+        confirmButtonText: "Lanjut ke WhatsApp"
+      }).then(() => {
+        // Ganti 'https://wa.me/<nomor>?text=<pesan>' dengan nomor dan pesan yang sesuai
+        window.location.href = "https://wa.me/6281234567890?text=Halo%20admin,%20saya%20ingin%20membeli%20produk%20ini.";
+      });
+    }
+  });
+}
